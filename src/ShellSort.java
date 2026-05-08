4f6f4990157be3b1a46361e9f3f586a15832942a
Anthony Gabriel <anthonygabrielsotto29@gmail.com>
2026-05-08 14:38:53 -0300

    refazendo shell sort com Knuth

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -2,25 +2,34 @@ public class ShellSort {
 
     public static void ordenar(int[] arr) {
         int n = arr.length;
+        int gap = 1;
 
-        // o algoritmo divide o array em subgrupos separados por um intervalo (gap).
-        // o gap começa sendo a metade do array e é dividido por 2 a cada ciclo, até chegar a 1.
-        for (int gap = n / 2; gap > 0; gap /= 2) {
+        // calcula o gap máximo inicial segundo a sequência de Knuth, a fórmula é h = 3h + 1
+        while (gap < n / 3) {
+            gap = 3 * gap + 1;
+        }
 
-            // lógica do Insertion Sort para os elementos que estão na distância do 'gap'.
+        // começa a ordenação e vai diminuindo o gap até chegar a 1
+        while (gap > 0) {
+
+            // lógica do Insertion Sort para os elementos na distância do 'gap'
             for (int i = gap; i < n; i++) {
 
-                int temp = arr[i]; // variável auxiliar para guardar o valor
-                int j;
+                int temp = arr[i];
+                int j = i;
 
-                // compara o valor guardado com os elementos anteriores do mesmo subgrupo.
-                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                // compara o valor guardado com os elementos anteriores do mesmo subgrupo
+                while (j >= gap && arr[j - gap] > temp) {
                     arr[j] = arr[j - gap];
+                    j -= gap; // volta o índice de acordo com o gap
                 }
 
-                // Insere o valor salvo na posição correta encontrada.
+                // insere o valor salvo na posição correta encontrada
                 arr[j] = temp;
             }
+
+            // reduz o gap para o próximo ciclo fazendo a fórmula inversa
+            gap = (gap - 1) / 3;
         }
     }
 }
